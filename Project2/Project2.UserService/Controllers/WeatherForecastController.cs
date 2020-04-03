1d693789ceaf81babaff0828cd2bbe6e8f05f41c
gazawayj <gazaway85@gmail.com>
2020-04-02 19:14:34 -0500

    add api create database

--- Project2/Project2.UserService/Controllers/WeatherForecastController.cs
+++ Project2/Project2.UserService/Controllers/WeatherForecastController.cs
@@ -26,7 +26,9 @@ namespace Project2.UserService.Controllers
           //string conString = ConsoleApplication1.Properties.Settings.Default.ConnectionString;
           using (SqlConnection con = new SqlConnection("server=sql_2;database=UserServiceDb;user id=sa;password=Password12345"))
           {
-            SqlCommand command = new SqlCommand("CREATE TABLE Customer(First_Name char(50),Last_Name char(50),Address char(50),City char(50),Country char(25),Birth_Date datetime);", con);
+            SqlCommand command = new SqlCommand("CREATE DATABASE MyDatabase", con);
+            con.Open();
+            command.ExecuteNonQuery();
           };
         }
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
