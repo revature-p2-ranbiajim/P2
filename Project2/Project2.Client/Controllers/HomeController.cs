bbb1b1aa57d80c4da51a445fb9ef915b404d9829
BiancaVisconti <bianca.bmvr@gmail.com>
2020-04-04 16:19:29 -0500

    testing api

--- Project2/Project2.Client/Controllers/HomeController.cs
+++ Project2/Project2.Client/Controllers/HomeController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Project2.MVC.Models;
+using Project2.UserService;
 
 namespace Project2.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient _http = new HttpClient();
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,7 +25,10 @@ namespace Project2.MVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var res = _http.GetAsync("http://api/weatherforecast").GetAwaiter().GetResult();
+            var weather = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(res.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+            
+            return View(weather);
         }
 
         public IActionResult Privacy()
