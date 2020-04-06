a09ff77f04ef46ad3910126bd2d1108466b38b5e
Randy Steinkamp <randy118@utexas.edu>
2020-04-06 14:38:20 -0500

    Change ensuregreated to migrate

--- Project2/Project2.GridService/Program.cs
+++ Project2/Project2.GridService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,7 +29,7 @@ namespace Project2.GridService
         {
             using (var dbContext = host.Services.GetRequiredService<GridDbContext>())
             {
-                dbContext.Database.EnsureCreated();
+                dbContext.Database.Migrate();
             }
             return host;
         }
