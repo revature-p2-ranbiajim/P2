7db2f7da722efec7740d153c22aa4893aaca1c9f
gazawayj <gazaway85@gmail.com>
2020-04-06 14:08:16 -0500

    add user repository as scoped service

--- Project2/Project2.UserService/Startup.cs
+++ Project2/Project2.UserService/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Project2.UserService.Storage;
+using Project2.UserService.Storage.Repositories;
 
 namespace Project2.UserService
 {
@@ -22,7 +23,9 @@ namespace Project2.UserService
         {
             services.AddControllers();
             services.AddDbContext<UserDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connection")));
-            services.AddCors(o => o.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod()));
+            services.AddScoped<UserRepository>();
+            // services.AddScoped<GridRepository>();
+            // services.AddCors(o => o.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
