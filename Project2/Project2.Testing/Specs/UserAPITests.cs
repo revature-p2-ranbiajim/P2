e812d8c40452468db1e6661f5575c33d5b50018c
BiancaVisconti <bianca.bmvr@gmail.com>
2020-04-06 17:04:45 -0500

    add test user API

--- Project2/Project2.Testing/Specs/UserAPITests.cs
+++ Project2/Project2.Testing/Specs/UserAPITests.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Logging;
 using Project2.UserService.Controllers;
+using Project2.UserService.Models;
+using Project2.UserService.Storage;
 using Project2.UserService.Storage.Repositories;
 using Xunit;
 
@@ -9,16 +11,26 @@ namespace Project2.Testing.Specs
   {
     
     private readonly UserRepository _ur;
+    private static UserDbContext _db;
     
     private readonly ILogger<UserController> logger = LoggerFactory.Create(o => o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<UserController>();
 
+    // [Theory]
+    // [InlineData("Randall1", "password")]
+    // public void Test_GetByUsernameAndPassword(string u, string p)
+    // {
+    //   var sut = new UserController(_ur);
+    //   var actual = sut.Get(u, p);
+    //   Assert.IsType<string>(actual);
+    // }
+
     [Theory]
     [InlineData("Randall1", "password")]
-    public void Test_GetByUsernameAndPassword(string u, string p)
+    public void Test_FindUser(string u)
     {
-      var sut = new UserController(_ur);
-      var actual = sut.Get(u, p);
-      Assert.IsType<string>(actual);
+      var sut = new UserRepository(_db);
+      var actual = sut.FindUser(u);
+      Assert.IsType<UserModel>(actual);
     }
   }
 }
