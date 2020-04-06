c5f458453f178301273b973cf3c33e09b1084611
BiancaVisconti <bianca.bmvr@gmail.com>
2020-04-05 21:08:14 -0500

    some testing

--- Project2/Project2.Testing/Specs/UserAPITests.cs
+++ Project2/Project2.Testing/Specs/UserAPITests.cs
@@ -6,15 +6,15 @@ namespace Project2.Testing.Specs
 {
   public class UserAPITests
   {
-    private readonly ILogger<UserController> logger = LoggerFactory.Create(o => o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<UserController>();
+    // private readonly ILogger<UserController> logger = LoggerFactory.Create(o => o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<UserController>();
 
-    [Theory]
-    [InlineData("Bianca", "bianca")]
-    public void Test_CreateUserReturnsView(string u, string p)
-    {
-      var sut = new UserController(logger);
-      var actual = sut.Get(u, p);
-      Assert.IsType<int>(actual);
-    }
+    // [Theory]
+    // [InlineData("Bianca", "bianca")]
+    // public void Test_CreateUserReturnsView(string u, string p)
+    // {
+    //   var sut = new UserController(logger);
+    //   var actual = sut.Get(u, p);
+    //   Assert.IsType<int>(actual);
+    // }
   }
 }
