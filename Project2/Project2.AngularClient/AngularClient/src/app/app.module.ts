a708bd918f0a31a997ee09e695250391ec046d18
Randy Steinkamp <randy118@utexas.edu>
2020-04-07 09:15:34 -0500

    Remove unnecessary code, change text

--- Project2/Project2.AngularClient/AngularClient/src/app/app.module.ts
+++ Project2/Project2.AngularClient/AngularClient/src/app/app.module.ts
@@ -4,17 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
-// import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
-    // ProfileEditorComponent
   ],
   imports: [
     BrowserModule,
-    // other imports ...
     ReactiveFormsModule
   ],
   providers: [],
