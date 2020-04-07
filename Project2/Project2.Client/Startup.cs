f299f5acc4cde77e8f2e9cec8f553e3fbfc673dd
gazawayj <gazaway85@gmail.com>
2020-04-07 10:35:08 -0500

    Add cors to user API

--- Project2/Project2.Client/Startup.cs
+++ Project2/Project2.Client/Startup.cs
@@ -24,12 +24,7 @@ namespace Project2.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            {
-            builder.AllowAnyOrigin()
-             .AllowAnyMethod()
-             .AllowAnyHeader();
-            }));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +40,6 @@ namespace Project2.MVC
             //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             //     app.UseHsts();
             // }
-            app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
--- Project2/Project2.UserService/Startup.cs
+++ Project2/Project2.UserService/Startup.cs
@@ -22,6 +22,12 @@ namespace Project2.UserService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+            }));
             services.AddDbContext<UserDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("main")));
             services.AddScoped<UserRepository>();
             // services.AddScoped<GridRepository>();
@@ -31,6 +37,7 @@ namespace Project2.UserService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,7 +47,6 @@ namespace Project2.UserService
 
             app.UseRouting();
 
-            // app.UseCors();
 
             app.UseAuthorization();
 
